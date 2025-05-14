f50d7cabcad157a1d50af9c533f2ef68c06eac81
Bina Kandel <bina.kandel@dohatec.net>
2025-05-14 23:11:35 +1000

    api services added

---
+++ server/services/model_service.py
@@ -0,0 +1,33 @@
+from db.config import SessionLocal
+from db.models import MalwareDetails
+import random
+import json
+
+db = SessionLocal()
+
+
+def get_overall_malware_details(malware_family: str, file_name: str):
+    details= db.query(MalwareDetails).filter(
+        MalwareDetails.malware_family == malware_family
+        ).first()
+
+    indices = generate_indices(file_name)
+    
+    info = json.loads(details.malware_info)
+    behaviour = json.loads(details.behaviour)
+    remedy = json.loads(details.remedy)
+
+    filtered_behaviour = [behaviour[i] for i in indices if 0 <= i < len(behaviour)]
+    filtered_remedy = [remedy[i] for i in indices if 0 <= i < len(remedy)]
+
+    return {
+        "malware_info": info,
+        "behaviour":filtered_behaviour,
+        "remedy":filtered_remedy
+        }   
+
+
+def generate_indices(input):
+    random.seed(input)
+    numbers = [random.randint(1, 10) for _ in range(4)]
+    return numbers
